27522c4e9cf01e32ba5a78af35f9f9753a0f6f48
Altimor <altimormc@gmail.com>
2023-10-15 19:18:29 -0400

    remove ball_update_surface_normals

--- src/game/monkey_ball.h
+++ src/game/monkey_ball.h
@@ -37,7 +37,6 @@ s32 ball_set_mario_rotation(struct MarioState *m);
 void ball_update_world_tilt(struct MarioState *m);
 void ball_update_floor_normal(struct MarioState *m, struct Surface *floor);
 void ball_update_wall_normal(struct MarioState *m, struct Surface *wall);
-void ball_update_surface_normals(struct MarioState *m);
 void ball_rotate_vector(struct MarioState *m, Vec3f out, Vec3f v, s32 invert);
 void ball_get_camera_transform(Mat4 transform, Mat4 rollMatrix, struct MarioState *m,
                                struct GraphNodeCamera *node);
