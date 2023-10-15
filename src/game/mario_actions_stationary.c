1e9247981a4e1387549faf9a3985d219d51da417
Altimor <altimormc@gmail.com>
2023-10-15 19:26:06 -0400

    not necessary to set yaw here

--- src/game/mario_actions_stationary.c
+++ src/game/mario_actions_stationary.c
@@ -87,7 +87,6 @@ s32 check_common_hold_idle_cancels(struct MarioState *m) {
     }
 
     if (m->input & (INPUT_NONZERO_ANALOG | INPUT_BALL_MOVING)) {
-        m->faceAngle[1] = (s16) m->intendedYaw;
         return set_mario_action(m, ACT_HOLD_WALKING, 0);
     }
 
